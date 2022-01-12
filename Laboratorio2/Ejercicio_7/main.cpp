fdf1dc28cb09cd62d2d9dc35d51baff2c70d1d33
jero521 <94910469+jero521@users.noreply.github.com>
2022-01-12 16:03:44 -0500

    se agrego al repositorio
    el ejercicio 5 y 7 al repositorio

---
+++ Laboratorio2/Ejercicio_7/main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+
+
+int main()
+{
+    char resultado[27]="";  //abecedario
+    char frase[50];
+
+    int i=0,j=0;
+    printf("Ingresa una frase: ");scanf("%s",frase);
+
+
+    //recore toda la frase
+    while(frase[i])
+    {
+        //busca la ocurrencia de un string
+        if(!strchr(resultado,frase[i]))
+            resultado[j++]=frase[i];
+        i++;
+    }
+    resultado[j]='\0'; //NULL (imprime hasta llegar ahi)
+    printf("\nEl resultado seria!\n%s",resultado);
+
+    getchar(); //funcion de una cadena de varios caracteres
+   return(0);
+}
